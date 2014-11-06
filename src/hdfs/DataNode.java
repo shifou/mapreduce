ee61240de41039244c313bad060d8be41bd94fb0
Druhin Sagar Goel <dsgoel@andrew.cmu.edu>
2014-11-05 19:42:08 -0500

    DataNode

--- src/hdfs/DataNode.java
+++ src/hdfs/DataNode.java
@@ -1,13 +1,45 @@
 package hdfs;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import main.Environment;
+
 public class DataNode  implements DataNodeRemoteInterface{
 
-	public DataNode(int dATA_NODE_REGISTRY_PORT) {
-		// TODO Auto-generated constructor stub
+	private int dataNodeRegistryPort;
+	private DataNodeRemoteInterface dataNodeStub;
+	private NameNodeRemoteInterface nameNodeStub;
+	
+	public DataNode(int dataNodeRegistryPort) {
+		this.dataNodeRegistryPort = dataNodeRegistryPort;
 	}
 
-	public void start() {
-		// TODO Auto-generated method stub
+	public boolean start() {
+		
+		if (!Environment.createDirectory()){
+			return false;
+		}
+		Registry reg;
+		try {
+			reg = LocateRegistry.getRegistry(Environment.Dfs.NAME_NODE_REGISTRY_PORT);
+			this.nameNodeStub = (NameNodeRemoteInterface)reg.lookup(Environment.Dfs.NAMENODE_SERVICENAME);
+			String serviceName = this.nameNodeStub.join(InetAddress.getLocalHost().getHostAddress());
+			this.dataNodeStub = (DataNodeRemoteInterface)UnicastRemoteObject.exportObject(this, 0);
+			reg.rebind(serviceName, this.dataNodeStub);
+		} catch (RemoteException | NotBoundException | UnknownHostException e) {
+			e.printStackTrace();
+			return false;
+		} 
+		return true;
+		
+		
 		
 	}
 
