6533a03240c6be0e80546423882dedc20c73c0bc
Druhin Sagar Goel <dsgoel@andrew.cmu.edu>
2014-11-08 21:14:45 -0500

    HDFS Working?

--- src/main/Environment.java
+++ src/main/Environment.java
@@ -40,9 +40,6 @@ public class Environment {
 
 			Document doc = dBuilder.parse(fXmlFile);
 
-			// optional, but recommended
-			// read this -
-			// http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
 			doc.getDocumentElement().normalize();
 			NodeList nList = doc.getElementsByTagName("dfs");
 			for (int temp = 0; temp < nList.getLength(); temp++) {
