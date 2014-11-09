c9fa0006b1c642079722dd1884f405421def9f12
Lixun Mao <mlx@Lixun.wv.cc.cmu.edu>
2014-11-08 19:46:08 -0500

    up

--- src/main/Environment.java
+++ src/main/Environment.java
@@ -10,7 +10,7 @@ public class Environment {
 		public static String NAMENODE_SERVICENAME="namenode";
 		public static int NAME_NODE_REGISTRY_PORT=11111;
 		public static int DATA_NODE_REGISTRY_PORT=22222;
-		public static String DIRECTORY="~/HDFS";
+		public static String DIRECTORY="HDFS";
 		public static int NAME_NODE_CHECK_PERIOD=5000;
 		public static int REPLICA_NUMS=2;
 		public static int BUF_SIZE=2048;
@@ -22,7 +22,7 @@ public class Environment {
 
 	public static boolean createDirectory(String name) {
 		
-			File folder = new File(Environment.Dfs.DIRECTORY+name);
+			File folder = new File(Environment.Dfs.DIRECTORY+"/"+name);
 			if (!folder.exists()) {
 				if (folder.mkdir()) {
 					System.out.println("Directory created");
