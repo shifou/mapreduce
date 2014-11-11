349da72dd21c0bd343d2aeaed64aca97cae839c6
Druhin Sagar Goel <dsgoel@andrew.cmu.edu>
2014-11-10 20:16:18 -0500

    Heartbeats for everyone

--- src/mapreduce/TaskTracker.java
+++ src/mapreduce/TaskTracker.java
@@ -1,5 +1,52 @@
 package mapreduce;
 
-public class TaskTracker {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import main.Environment;
+
+public class TaskTracker implements TaskTrackerRemoteInterface {
+
+	private String serviceName;
+	private JobTrackerRemoteInterface jobTrackerStub;
+	private TaskTrackerRemoteInterface taskTrackerStub;
+	
+	public TaskTracker(){
+		
+	}
+	
+	public boolean start(){
+		
+		
+		try {
+			
+			Registry reg = LocateRegistry.getRegistry(Environment.Dfs.NAME_NODE_IP, Environment.Dfs.NAME_NODE_REGISTRY_PORT);
+			this.jobTrackerStub = (JobTrackerRemoteInterface)reg.lookup(Environment.MapReduceInfo.JOBTRACKER_SERVICENAME);
+			this.serviceName = this.jobTrackerStub.join(InetAddress.getLocalHost().getHostAddress());
+			if (!Environment.createDirectory(this.serviceName)){
+				return false;
+			}
+			this.taskTrackerStub = (TaskTrackerRemoteInterface)UnicastRemoteObject.exportObject(this, 0);
+			Registry r = LocateRegistry.getRegistry(Environment.Dfs.DATA_NODE_REGISTRY_PORT);
+			r.rebind(this.serviceName, this.taskTrackerStub);
+			
+		} catch (RemoteException | NotBoundException | UnknownHostException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+		
+	}
+
+	@Override
+	public void healthCheck(Boolean b) throws RemoteException {
+	
+		
+	}
 	
 }
