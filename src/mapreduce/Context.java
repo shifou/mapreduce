e39574b33cc201f7f6769d47caef62675615c592
Lixun Mao <mlx@Lixun.wv.cc.cmu.edu>
2014-11-11 01:19:56 -0500

    up

--- src/mapreduce/Context.java
+++ src/mapreduce/Context.java
@@ -1,13 +1,37 @@
 package mapreduce;
 
-public class Context<K extends Writable, V extends Writable> {
-		
-		private Priority_Queue<KeyValue<K, V>> keyvalueQueue;
+import java.io.Serializable;
+import java.util.PriorityQueue;
 
-		public void write(Text word, IntWritable one) {
-			// TODO Auto-generated method stub
-			
-		} 
-		
-	
+public class Context<K extends Writable, V extends Writable> {
+
+	private PriorityQueue<Entry> ans;
+
+	private class Entry<Key extends Writable, Value extends Writable>
+			implements Comparable {
+		private Key key;
+		private Value value;
+
+		public Entry(Key key, Value value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		public Key getKey() {
+			return this.key;
+		}
+
+		public Value getValue() {
+			return this.value;
+		}
+
+		@Override
+		public int compareTo(Object o) {
+			return this.key.hashCode() - ((Entry) o).getKey().hashCode();
+		}
+	}
+
+	public void write(Text word, IntWritable one) {
+
+	}
 }
