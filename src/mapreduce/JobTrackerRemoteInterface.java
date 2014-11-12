800bdfb0288fea608ad51dfe1f6242325946e153
Lixun Mao <mlx@Lixun.wv.cc.cmu.edu>
2014-11-12 01:05:45 -0500

    up

--- src/mapreduce/JobTrackerRemoteInterface.java
+++ src/mapreduce/JobTrackerRemoteInterface.java
@@ -7,4 +7,5 @@ public interface JobTrackerRemoteInterface extends Remote {
 
 	public String join(String IP) throws RemoteException;
 	public String putJar(String jobid, String jarname, Byte []arr, int ct) throws RemoteException;
+	public String getJar(String jobid)
 }
